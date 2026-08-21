d59783abb22a599acebb08618b7892155364c59b
Architect Sys Agent <businesspymes2020@gmail.com>
2026-08-21 21:24:55 +0000

    fix: Final TS strict fixes

--- src/components/IngredientsModal.tsx
+++ src/components/IngredientsModal.tsx
@@ -1,3 +1,4 @@
+import { Product } from '../data/products';
 import { useState, useMemo } from 'react';
 import { NESTOR_INGREDIENTS_OFICIAL } from '../data/products';
 import { useCartStore } from '../store/cartStore';
