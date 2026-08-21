0f01cbc1226ef633b335bf96e0d74faf926a623c
Architect Sys Agent <businesspymes2020@gmail.com>
2026-08-21 21:21:33 +0000

    fix: Type import mismatches

--- src/components/KioskIngredientsModal.tsx
+++ src/components/KioskIngredientsModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { NESTOR_INGREDIENTS_OFICIAL, Product } from '../data/products';
+import { NESTOR_INGREDIENTS_OFICIAL } from '../data/products';
+import { Product } from '../types';
 import { CartItem } from '../store/cartStore';
 import { useHardwareBack } from '../utils/useHardwareBack';
 import { useI18nStore } from '../store/i18nStore';
