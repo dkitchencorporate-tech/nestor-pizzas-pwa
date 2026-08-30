28112b399acfb13d5f33ad837854737595cbd4a5
dkitchencorporate-tech <dkitchencorporate@gmail.com>
2026-08-30 14:00:19 +0000

    security(sanitization): purge exposed script check_db.cjs

--- check_db.cjs
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('https://jlchjamoejkzahaeimec.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsY2hqYW1vZWpremFoYWVpbWVjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NjU1NjMzMCwiZXhwIjoyMTAyMTMyMzMwfQ.s6GyK7sIowzYolQzsRJYr8GEVoWbN4m-znX51V1rTMs');
-
-async function run() {
-  const { data, error } = await supabase.from('products').select('*');
-  console.log(data ? data.map(d => d.name) : error);
-}
-run();
